24e9047800177490b0685acc9825a9268cc45dda
Jake Biddlecome <jake.biddlecome@gmail.com>
2025-11-19 15:00:12 -0800

    Move resume analyzer to dedicated page

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from apps.employee_phone_county_audit.views import router as employee_audit_rout
 from apps.health_benefits.views import router as health_benefits_router
 from apps.interview_questions.views import router as interview_questions_router
 from apps.recruiting_metrics.views import router as recruiting_metrics_router
+from apps.resume_analyzer.views import router as resume_analyzer_router
 from apps.sales_staffing_metrics.views import router as sales_staffing_router
 from apps.text_blast_filter.views import router as text_blast_router
 from apps.ucla_hours_tool.views import router as ucla_hours_router
@@ -37,6 +38,7 @@ app.include_router(clickboarding_router, prefix="/clickboarding-check", tags=["C
 app.include_router(health_benefits_router, prefix="/health-benefits", tags=["Health Benefits"])
 app.include_router(sales_staffing_router, prefix="/sales-staffing-metrics", tags=["Sales & Staffing Metrics"])
 app.include_router(recruiting_metrics_router, prefix="/recruiting-metrics", tags=["Recruiting Metrics"])
+app.include_router(resume_analyzer_router, prefix="/resume-analyzer", tags=["Resume Analyzer"])
 app.include_router(concierge_router, prefix="/concierge", tags=["Concierge"])
 app.include_router(text_blast_router, prefix="/text-blast-filter", tags=["Text Blast Filter"])
 app.include_router(ucla_hours_router, prefix="/ucla-hours-tool", tags=["UCLA Hours Tool"])
